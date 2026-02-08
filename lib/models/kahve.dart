a7027ceb435e18a4968ba63eff75fc6e7f97c0ef
rana eyp <eyrana002@gmail.com>
2026-02-08 14:00:40 +0300

    Update kahve model (puan ve yorumSayisi)

--- lib/models/kahve.dart
+++ lib/models/kahve.dart
@@ -2,7 +2,14 @@ class Kahve {
   String isim;
   double fiyat;
   String resimYolu;
-  String kategori;
+  double puan;        
+  int yorumSayisi;    
 
-  Kahve({required this.isim, required this.fiyat, required this.resimYolu,required this.kategori});
+  Kahve({
+    required this.isim,
+    required this.fiyat,
+    required this.resimYolu,
+    required this.puan,        
+    required this.yorumSayisi, 
+  });
 }
