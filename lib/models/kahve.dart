ea47347e495eb4889bb2609faf9b29aed0f9b46b
rana eyp <eyrana002@gmail.com>
2026-02-11 20:08:58 +0300

    add sepet.dart

--- lib/models/kahve.dart
+++ lib/models/kahve.dart
@@ -3,14 +3,10 @@ class Kahve {
   double fiyat;
   String resimYolu;
   String kategori;
-  double puan;
-  int yorumSayisi;
   Kahve({
     required this.isim,
     required this.fiyat,
     required this.resimYolu,
     required this.kategori,
-    required this.puan,
-    required this.yorumSayisi,
   });
 }
