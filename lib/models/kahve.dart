801ef41587a52f570a652529b3d965997c60fb2a
rana eyp <eyrana002@gmail.com>
2026-02-14 00:47:36 +0300

    kahve.dart geri yüklendi

---
+++ lib/models/kahve.dart
@@ -0,0 +1,12 @@
+class Kahve {
+  String isim;
+  double fiyat;
+  String resimYolu;
+  String kategori;
+  Kahve({
+    required this.isim,
+    required this.fiyat,
+    required this.resimYolu,
+    required this.kategori,
+  });
+}
