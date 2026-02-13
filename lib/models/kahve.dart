16d3c8d94084b670aeedad3b465e0912e71326d7
rana eyp <eyrana002@gmail.com>
2026-02-14 01:12:19 +0300

    update kahve.dart

--- lib/models/kahve.dart
+++ lib/models/kahve.dart
@@ -3,10 +3,12 @@ class Kahve {
   double fiyat;
   String resimYolu;
   String kategori;
+  bool isFavorite=false;
   Kahve({
     required this.isim,
     required this.fiyat,
     required this.resimYolu,
     required this.kategori,
+    required this.isFavorite,
   });
 }
